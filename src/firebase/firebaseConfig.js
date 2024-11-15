17e4072bda1a96a88b81968e15601db771dd3353
Dnyaneshwar Patange <114939884+dnyaneshwarpatange@users.noreply.github.com>
2024-11-16 01:39:18 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,14 +4,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Firebase configuration object
-const firebaseConfig = {
-  apiKey: "AIzaSyBq3WP0Qn0EAJLIaV5wrOuleTaSzkdvJi4",
-  authDomain: "cloudcube-637d9.firebaseapp.com",
-  projectId: "cloudcube-637d9",
-  storageBucket: "cloudcube-637d9.appspot.com",
-  messagingSenderId: "179693374688",
-  appId: "1:179693374688:web:cf08969c699ea1e9e69485",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
